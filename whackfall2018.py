8aec9397a613798b24ce4ce38a60b78f3d803640
Jun Ru Anderson <junruanderson@Juns-MacBook-Air.local>
2018-11-02 21:07:23 -0400

    work on first page

--- whackfall2018.py
+++ whackfall2018.py
@@ -8,5 +8,22 @@ def default():
     return render_template('index.html')
 
 
+@app.route('/missionstatement')
+def missionstatement():
+    return render_template('missionstatement.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+
 if __name__ == '__main__':
     app.run()
