5bf4c520705068ba27a98dc941782b17e6d01d93
Jun Ru Anderson <junruanderson@Juns-MacBook-Air.local>
2018-11-04 01:29:21 -0500

    test

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import json, os, hashlib
 from flask_mail import Mail, Message
+from pathlib import Path
 import updateDB
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-UPLOAD_FOLDER = 'usrpics'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'jpg', 'json'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config.update(dict(
@@ -46,12 +47,12 @@ def resources():
     return render_template('resources.html')
 
 
-@app.route('/profile')
-def profile():
-    with open('junruanderson.json', 'r') as file:
+@app.route('/profile/<filename>')
+def profile(filename):
+    with open(filename + ".json", 'r') as file:
         d = json.loads(file.read())
     return render_template('profile.html', name=d['name'], tagline=d['tagline'], about=d['about'],
-                           imgsrc="../static/usrpics/" + "test.png", causes=d['causes'],
+                           imgsrc=filename + ".jpg", causes=d['causes'],
                            socialmedia=d['socialmedia'],
                            keywords=d['keywords'])
 
@@ -64,16 +65,29 @@ def createprofile():
 def makeprofile():
     filename = request.form['firstname'] + request.form['lastname']
     img = request.files['pic']
-    img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + os.path.splitext(img.filename)[1]))
+    json_filename = filename + ".json"
+    #img_filename = Path(os.path.join(app.config['UPLOAD_FOLDER'], filename + ".jpg"))
+
+    i = 0
+    changed = False
+    while Path(json_filename).exists():
+        changed = True
+        i += 1
+        json_filename = filename + str(i) + ".json"
+
+    """if changed:
+        img_filename = Path(os.path.join(app.config['UPLOAD_FOLDER'], filename + str(i) + ".jpg"))"""
+
+    #img.save(img_filename)
     socialmedia=request.form['socialmedia'].split("\n")
     d = {'name': request.form['firstname'] + " " + request.form['lastname'],
-         'passhash': hashlib.sha3_256((request.form['firstname'] + request.form['lastname'] + request.form['password']).encode(encoding='utf-8')),
+         #'passhash': hashlib.sha3_256((request.form['firstname'] + request.form['lastname'] + request.form['password']).encode(encoding='utf-8')),
          'tagline': request.form['tagline'],
          'about': request.form['about'],
          'causes': request.form['causes'],
-         'keywords': [request.form['firstname'], request.form['lastname'], request.form['firstname'] + " " + request.form['lastname'], request.form['tagline']],
+         'keywords': [request.form['firstname'], request.form['lastname'], request.form['firstname'] + " " + request.form['lastname'], request.form['tagline'], "songbyrd"],
          'socialmedia': socialmedia}
-    with open("./" + filename + ".json", "w") as file:
+    with open(json_filename, "w") as file:
         file.write(json.dumps(d))
     return redirect(url_for('success'), code=302)
 
